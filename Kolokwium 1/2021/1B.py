b21ff368eb479bc8fedefcca331e6156f57caf6f
Marcin Serafin <Marcin.serafin0325@gmail.com>
2023-11-12 14:32:30 +0100

    Update 1B.py

--- Kolokwium 1/2021/1B.py
+++ Kolokwium 1/2021/1B.py
@@ -8,6 +8,8 @@
 # Jeżeli są sobie równe to sprawdzamy czy ta liczba jest większa od maxa i w takim wypadku maxa zamieniamy na tą liczbe
 # A pod zmienną przechowującą indeks podstawiamy indeks tej liczby
 # Na koniec zwaracamy indeks największej liczby spełniającej warunki zadania
+
+
 def is_prime(num):
     if num> 1:  
         for n in range(2,num):  
@@ -16,6 +18,9 @@ def is_prime(num):
         return True
     else:
         return False
+
+
+
 def zad1(T):
     n=len(T)
     maxi=0
@@ -30,6 +35,8 @@ def zad1(T):
                 ind=i
     return ind
 
-T=[2,4,5,7,70,7,2,980,1000]
+
+
+T=[2,4,5,7,70,7,2,980,1000] #7
 
 print(zad1(T))
