ebbe115453665036a6ddf86ae754797e4d0c5382
Marcin Serafin <Marcin.serafin0325@gmail.com>
2023-11-24 13:27:22 +0100

    Opis

--- Zestawy/06/7.py
+++ Zestawy/06/7.py
@@ -1,3 +1,24 @@
+### BIT ALGO START
+### Autor: Marcin Serafin
+
+# Idea:
+# Tworze funkcje rek którą bede rekurencyjnie wyknywał
+# Jak zawsze najpierw pisze warunki końca tj.
+# Jeżeli zbiore odpowiednia mase zwracam True
+# Jeżeli przejde przez całą tablice a masa dalej nie jest równa 0 zwracam False
+# Case zad 7: 
+#   Odpalam Funkcje i albo biore odważnik odejmując go od całej masy i przesuwam indeks +1
+#   albo nie biore i przesuwam indeks +1
+#
+# Case zad 8:
+# Jak wyżej ale dodaje kolejną możliwość wywowania rek
+# Tym razem kładąc na drugą X co oznacza dodanie wagi odważnika do masy
+#
+# Case zad 9:
+# Jak wyżej ale jeżeli dojde do masy równej 0 do printuje tablice którą przekazuje rekurencyjnie w rek
+# Dodaje do tej tablicy za pomocą łączenia tablic
+# Do już stworzonej dodaje jednoelemntową z wagą odważnika (Bo nie wolno appenda)
+
 def zad7(T,m):
     def rek(T,i,m):
         if m==0:
@@ -7,6 +28,8 @@ def zad7(T,m):
         return rek(T,i+1,m-T[i]) or rek(T,i+1,m)
     return rek(T,0,m)
 
+
+
 def zad8(T,m):
     def rek(T,i,m):
         if m==0:
@@ -15,6 +38,9 @@ def zad8(T,m):
             return False
         return rek(T,i+1,m-T[i]) or rek(T,i+1,m) or rek(T,i+1,m+T[i])
     return rek(T,0,m)
+
+
+
 def zad9(T,m):
     def rek(T,i,m,chosen):
         if m==0:
@@ -22,12 +48,12 @@ def zad9(T,m):
             return True
         if m<0 or i>=len(T):
             return False
-        if(rek(T,i+1,m-T[i],chosen+[T[i]]) or rek(T,i+1,m+T[i],chosen+[T[i]]) or rek(T,i+1,m,chosen)):
-            return True
+        return rek(T,i+1,m-T[i],chosen+[T[i]]) or rek(T,i+1,m+T[i],chosen+[T[i]]) or rek(T,i+1,m,chosen)
+            
     return rek(T,0,m,[])
     
 
 T=[2,3,7,2]
-print(zad7(T,6))
-print(zad8(T,6))
-print(zad9(T,6))
+print(zad7(T,6)) #False
+print(zad8(T,6)) #True
+print(zad9(T,6)) #[2,3,7] True
--- Zestawy/06/noname.py
+++ Zestawy/06/noname.py
@@ -1,3 +1,15 @@
+### BIT ALGO START
+### Autor: Marcin Serafin
+
+# Idea:
+# Klasycznie tworze funkcje rek zawierającą obie tablice, współrzedne figury, licznik oraz poprzedni ruch
+# Poprzedni ruch potrzebny jest zeby nie zapętlić sie chodząc góra dół
+# Jako warunki końca wybieram dojście do prawego górnego rogu
+# Jak dojde to zwracam licznik
+# Sprawdzam gdzie moge sie ruszyć nie wypadając poza tablice i nie wchodząc na pionka (not in)
+# Porównuje maksymalny licznik(zmienna maxi) z kolejny wywołaniem (ruchem)
+# Nie bierzemy pod uwage czy w ogóle da sie dojść do prawego dolnego rogu
+
 def zad(T,P):
     def rek(T,P,y,x,cnt,prev):
         maxi=0
@@ -14,5 +26,5 @@ def zad(T,P):
         return maxi
     return(rek(T,P,0,0,T[0][0],None))
 P=[(1,1)]
-T=[[1,1,1],[1,2,1],[1,3,1]]
+T=[[1,1,1],[1,2,13],[1,3,1]]
 print(zad(T,P))
