aa6b562c7178ea64f075cc3d04465404eb264d7c
Marcin Serafin <Marcin.serafin0325@gmail.com>
2023-11-24 17:03:06 +0100

    Ostatnie szlify

--- Kolokwia/22-23/kol2a/3.py
+++ Kolokwia/22-23/kol2a/3.py
@@ -15,14 +15,18 @@ def king(N,L):
         maxi=0
         if y==N-1 and x==N-1:
             return cnt
+        
         if x<N-1 and (y,x+1) not in L:
             maxi=max(maxi,rek(N,L,y,x+1,cnt+1,None))
+
         if y<N-1 and (y+1,x) not in L:
              if prev!="down":
                 maxi=max(maxi,rek(N,L,y+1,x,cnt+1,"up"))
+
         if y>0 and (y-1,x) not in L:
             if prev!="up":
                 maxi=max(maxi, rek(N,L,y-1,x,cnt+1,"down"))
+                
         return maxi
     return(rek(N,L,0,0,0,None))
 L=[(0,1)]
